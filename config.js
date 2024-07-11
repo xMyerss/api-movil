c25900dc2f7aca2317a1417157d8f74bbb30c586
xMyerss <jos_t20@outlook.com>
2024-07-11 08:55:26 -0600

    update

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+export const DB_HOST = process.env.DB_HOST || "3.223.118.103";
+export const DB_USER = process.env.DB_USER || "josafat";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "josafat020";
 export const DB_DATABASE = process.env.DB_DATABASE || "movil";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const API_HOST = process.env.API_HOST || "localhost:3306";
